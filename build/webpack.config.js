ab7bbb994632e1149fab835e93c4a06490429d91
nxl3477 <nxl3477@gmail.com>
2019-02-13 18:21:27 +0800

    支持 Vue 双向绑定

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,13 +1,42 @@
-const { join } = require('path')
-
-
+const { join, resolve } = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path: join(__dirname, '../dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: '../dist', //必须加publicPath
+  },
+  module: {
+    rules: [
+      // ... 其它规则
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        // 过滤node_modules
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      template: 'index.html'
+    }),
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    host: 'localhost', //可选，ip
+    port: 3000, //可选，端口,
+    hot: true,
+    inline: true,
+    contentBase: resolve(__dirname,'dist'), //可选，基本目录结构
+    compress: true //可选，压缩
   }
-
-
 }
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>as2d1a</h2>
+    Hello world {{txt}}
+    <div>
+      <input type="text" v-model="txt" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      txt: 'ok'
+    }
+  }
+}
+</script>
+<style>
+
+</style>
